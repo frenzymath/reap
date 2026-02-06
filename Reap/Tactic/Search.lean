69fdea9ff07e80d820072882d62da19c6b442fbe
slashbade <slashbladevitali@gmail.com>
2026-02-06 19:31:14 +0800

    feat: remove aesop deps

--- Reap/Tactic/Search.lean
+++ Reap/Tactic/Search.lean
@@ -1,18 +1,7 @@
 module
-public meta import Aesop
 public meta import Reap.Tactic.Generator
-public meta import Reap.Tactic.Ruleset
+public meta import TreeSearch
 
 public meta section
 
-@[aesop 100% (rule_sets := [reap])]
-def reapTacGen : Aesop.TacGen := TacticGenerator.generateTactics
-
-elab "reap!!" : tactic => do
-  let opts ← Lean.getOptions
-  let maxGoals := reap.max_goals.get opts
-  let maxGoalsStx := Lean.Syntax.mkNumLit (toString maxGoals)
-  Lean.Elab.Tactic.evalTactic (← `(tactic| aesop? (config := {
-    enableSimp := false,
-    enableUnfold := false,
-    maxGoals := $maxGoalsStx}) (rule_sets := [reap])))
+elab "reapBFS" : tactic => proofSearchBFS TacticGenerator.generateTactics
