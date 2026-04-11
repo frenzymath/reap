2efdf62702bb069f0d6ed46184ea72a309f6f86c
Tony Beta Lambda <tonybetalambda@gmail.com>
2026-04-11 20:42:09 +0800

    chore: disable over-smart Lean devs

--- Reap/Tactic/TestBFS.lean
+++ Reap/Tactic/TestBFS.lean
@@ -1,4 +1,6 @@
 import Reap.Tactic.Syntax
 
+set_option linter.unusedSimpArgs false
+
 example : (a b : Nat) → a = b → b = a := by
   reapBFS
--- Reap/Tactic/TestMCTS.lean
+++ Reap/Tactic/TestMCTS.lean
@@ -1,4 +1,6 @@
 import Reap.Tactic.Syntax
 
+set_option linter.unusedSimpArgs false
+
 example : (a b c : Nat) → a = b → b = c → a = c := by
   reapMCTS
