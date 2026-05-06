e49e0d6edc6e80ab6f88c7a13876300c4b0ea734
Tony Beta Lambda <tonybetalambda@gmail.com>
2026-05-06 22:39:38 +0800

    feat: output Q, U, c
    Co-authored-by: Xuanzhi Ren <1050539140@qq.com>

--- Reap/Tactic/TreeSearch.lean
+++ Reap/Tactic/TreeSearch.lean
@@ -77,7 +77,7 @@ structure NodeData where
 
 namespace NodeData
 def value (node : NodeData) : Float :=
-  node.valueSum / node.numVisit.toFloat
+  if node.numVisit == 0 then 0.0 else node.valueSum / node.numVisit.toFloat
 
 def isTerminal (node : NodeData) : TacticM Bool :=
   TreeSearch.isSolved node.state
@@ -86,21 +86,6 @@ def restore (node : NodeData) : TacticM Unit :=
   node.state.restore
 
 end NodeData
-
-def ppNodeData (node : NodeData) : TacticM Json := do
-  node.state.restore
-  let pp ← (← getUnsolvedGoals).mapM fun g => do return toString (← Meta.ppGoal g)
-  return json%{
-    state: $(pp),
-    valueSum: $(node.valueSum),
-    numVisit: $(node.numVisit)
-  }
-
-def ppNode {ε} [ToJson ε] (node : Node NodeData ε) : TacticM Json := do
-  return json%{
-    data: $(← ppNodeData node.data),
-    children: $(node.children)
-  }
 
 structure EdgeData where
   tacticStr : String
@@ -126,7 +111,7 @@ def visitNode (tg : TacGen) (se : StateEval) (node : NodeData) : TacticM (NodeDa
     if let some s' ← evalTacticStr t (reap.heartbeats.get (← getOptions)) then
       -- TODO: merge nodes
       s'.restore
-      children := children.push (⟨ t, ps, prior.exp, 0, 0 ⟩, ⟨ s', 0.0, node.numVisit ⟩)
+      children := children.push (⟨ t, ps, prior.exp, 0, 0 ⟩, ⟨ s', 0.0, 0 ⟩)
 
   return (node', children)
 
@@ -136,7 +121,13 @@ abbrev NodeType := Node NodeData (EdgeData × NodeData)
 def scaledNatToFloat (n : Nat) : Float :=
   n.toFloat / 1000.0
 
-def computePUCTScores (node : NodeType) : TacticM (Array Float) := do
+structure CQU where
+  c : Float
+  Q : Float
+  U : Float
+deriving Inhabited, ToJson
+
+def computePUCTScores (node : NodeType) : TacticM (Array (Float × CQU)) := do
   let opts ← getOptions
   let cBase := reap.c_base.get opts |>.toFloat
   let cInit := scaledNatToFloat (reap.c_init.get opts)
@@ -149,7 +140,37 @@ def computePUCTScores (node : NodeType) : TacticM (Array Float) := do
   return node.children.map fun (e, n) =>
     let Q := Float.pow visitDiscount (n.value - 1)
     let U := c * Float.pow e.probability (1 / priorTemperature) * N.sqrt / (e.numVisit.toFloat + 1)
-    Q + U
+    let score := Q + U
+    (score, { c := c, Q := Q, U := U })
+
+def ppNodeData (node : NodeData) : TacticM Json := do
+  node.state.restore
+  let pp ← (← getUnsolvedGoals).mapM fun g => do return toString (← Meta.ppGoal g)
+  return json%{
+    state: $(pp),
+    valueSum: $(node.valueSum),
+    numVisit: $(node.numVisit)
+  }
+
+def ppNode (arr : Array (Node NodeData (EdgeData × Nat))) (node : Node NodeData (EdgeData × Nat)) : TacticM Json := do
+  let children ← node.children.mapM fun (e, i) => do
+    let some x := arr[i]? | unreachable!
+    return (e, x.data)
+  let scores ← computePUCTScores { data := node.data, children := children }
+  let ppChildren := node.children.zip scores |>.map fun ((e, i), (score, cqu)) => json%{
+    edge: $e,
+    extra: {
+      score: $score,
+      c: $cqu.c,
+      Q: $cqu.Q,
+      U: $cqu.U
+    },
+    childIndex: $i
+  }
+  return json%{
+    data: $(← ppNodeData node.data),
+    children: $ppChildren
+  }
 
 def selectChild (node : NodeType) : TacticM (Option Nat) := do
   let scores ← computePUCTScores node
@@ -157,7 +178,7 @@ def selectChild (node : NodeType) : TacticM (Option Nat) := do
   return Id.run do
     let mut bestIdx := none
     let mut bestScore := -Float.inf
-    for (score, i) in scores.zipIdx do
+    for ((score, _), i) in scores.zipIdx do
       if score > bestScore then
         bestIdx := some i
         bestScore := score
@@ -177,11 +198,6 @@ def updateNode (node : NodeType) (leaf : NodeData) : NodeData :=
       valueSum := node.data.valueSum + leaf.valueSum
   }
 
-structure PPNodeData where
-  pp : List String
-  value : Float
-deriving ToJson
-
 end MCTS
 
 open MCTS in
@@ -198,7 +214,7 @@ def reapMCTS (tg : TacGen) (se : StateEval)
       ⟨ (← Tactic.saveState), 0.0, 0 ⟩
       maxNodes maxSteps
 
-    let ppNodes ← nodes.mapM ppNode
+    let ppNodes ← nodes.mapM (ppNode nodes)
     let info := json%{
       solution : $k,
       nodes : $ppNodes
